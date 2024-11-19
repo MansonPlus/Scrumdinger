7f2241941b7f86f178ab24d79f0353f68c8d09fc
Juan Manuel Gomez <juan@lucci.ai>
2024-11-19 12:59:12 -0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Juan Manuel Gomez Llanos on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
